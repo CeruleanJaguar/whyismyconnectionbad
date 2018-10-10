c7e7e5e3e5a551f1776c2c21dac952214ff28e57
CeruleanJaguar <theo.lawson@oliverwyman.com>
2018-10-10 15:10:49 +0100

    Add pingers for all args, report statistics on program termination.
    Currently doesn't block so it exits instantly. Intending to block on a channel of keyboard inputs
    so that statistics can be reported on without killing the process entirely upon pressing a specific
    key combo, and allow the user to end the process with a different one.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -10,6 +11,28 @@ import (
 	"github.com/jackpal/gateway"
 	"github.com/sparrc/go-ping"
 )
+
+func runPing(addr string) (p *ping.Pinger, err error) {
+	p, err = ping.NewPinger(addr)
+
+	if err != nil {
+		return
+	}
+
+	p.SetPrivileged(true)
+	p.Interval = time.Second
+	p.Count = -1
+
+	go p.Run()
+
+	return
+}
+
+func printStats(name string, s *ping.Statistics) {
+	fmt.Printf(
+		"\n\nStatistics for %s (%d packets sent):\n\t- Packet Loss: %v%%\n\t- Avg. RTT (ms): %v\n\t- Min. RTT (ms): %v\n\t- Max. RTT (ms): %v\n\t- Std. Dev. RTT (ms): %v\n",
+		name, s.PacketsSent, s.PacketLoss, s.AvgRtt, s.MinRtt, s.MaxRtt, s.StdDevRtt)
+}
 
 func main() {
 	fmt.Println("Attempting to resolve the default gateway...")
@@ -23,7 +46,7 @@ func main() {
 
 	fmt.Printf("Default gateway resolved to %s\n", dg.String())
 
-	sites := os.Args[1:]
+	sites := flag.Args()
 	var valid []string
 
 	for i, site := range sites {
@@ -47,19 +70,34 @@ func main() {
 			fmt.Println("Cannot resolve default external site (google.com), exiting...")
 			os.Exit(1)
 		}
+
+		fmt.Println("Default site resolved, pinging default gateway and the default site.")
+		valid = []string{"google.com"}
 	}
 
-	fmt.Println("Default site resolved, pinging default gateway and the default site.")
-	valid = []string{"google.com"}
-
-	dgPing, dgErr := ping.NewPinger(dg.String())
-
-	if dgErr != nil {
-		fmt.Printf("Error creating DG ping: %s", dgErr)
+	if dgPing, dgPErr := runPing(dg.String()); dgPErr != nil {
+		fmt.Printf("Couldn't set up pinger for the Default Gateway:\n\t`%s`\nExiting...", dgPErr)
 		os.Exit(1)
-	}
+	} else {
+		pingers := []*ping.Pinger{}
+		for _, site := range valid {
+			if p, err := runPing(site); err != nil {
+				fmt.Printf("Couldn't set up pinger for %s:\n\t`%s`\nExiting...", site, err)
+				os.Exit(1)
+			} else {
+				pingers = append(pingers, p)
+			}
+		}
 
-	dgPing.SetPrivileged(true)
-	dgPing.Interval = time.Second
-	dgPing.Count = -1
+		reportStats := func() {
+			dgName := fmt.Sprintf("Default Gateway (%s)", dg.String())
+			printStats(dgName, dgPing.Statistics())
+
+			for i, pinger := range pingers {
+				printStats(valid[i], pinger.Statistics())
+			}
+		}
+
+		defer reportStats()
+	}
 }
